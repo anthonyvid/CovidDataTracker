f41a2de31730c54f1e5da2aa98e34719ead3f033
Anthony Vidovic <avidovic@uoguelph.ca>
2021-07-08 23:50:03 -0400

    project setup

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = require("./router");
+const expressLayouts = require("express-ejs-layouts");
+
+const app = express();
+
+console.log("in App.js");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+// app.set("layout", "layouts/layout");
+// app.use(expressLayouts);
+app.use(router);
+
+app.listen(4000, () => {
+	console.log("server start on port 4000");
+});
+
+module.exports = app;
---
+++ controller.js
@@ -0,0 +1,3 @@
+exports.homePage = function (req, res) {
+	res.render("home");
+};
